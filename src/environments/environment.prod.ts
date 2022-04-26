b9f2c3355629af8b8b1e935612c56029aa52fad2
Jackecx02 <jacobcuevas181@gmail.com>
2022-04-26 17:55:16 -0400

    Modified production file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'http://localhost',
+  API_URL_PORT: 8080,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   API_URL: 'http://localhost',
-  API_URL_PORT: 3000,
+  API_URL_PORT: 8080,
 };
